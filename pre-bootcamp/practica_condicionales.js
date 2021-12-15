adda172774da1619e3f2d17ce0eb77a8856c37f6
Smoyacs <smoya.cs@gmail.com>
2021-12-15 17:52:01 -0300

    movement settings(0,500)

--- pre-bootcamp/practica_condicionales.js
+++ pre-bootcamp/practica_condicionales.js
@@ -25,4 +25,54 @@ console.log(num2);
 
 // 20
 // 15
+function maxMinAvg(arr) {
+    var arrnew=[];
+    var max=0;
+    var ln=arr.length;
+    var sum=0;
 
+
+    
+    for(var i=0;i<ln;i++){
+
+        if(arr[i]>max){
+            max = arr[i];
+        }
+        
+        if(arr[i]<max){
+            min = arr[i];
+        }
+    
+        sum = sum + arr[i];
+
+    }
+
+
+    
+    avg = sum/ln
+    
+    arrnew.push(max);
+    arrnew.push(min);
+    arrnew.push(avg);
+    
+    return console.log(arrnew); 
+}
+var arr=[4,2,5,87,-3,0,-100];
+maxMinAvg(arr);
+
+var arr=[100,200,1,300,500,20];
+function swap(arr) {
+    
+    var arrnew=0;
+    var temp=0;
+    var ln=arr.length;
+    
+    temp=arr[0];
+    arr[0]=arr[ln-1];
+    arr[ln-1]=temp;
+    arrnew=arr
+
+    return console.log(arrnew); 
+}
+
+swap(arr);
