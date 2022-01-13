26137defb965cbf80b3f5cc926c5c2f967554df7
Smoyacs <95979752+Smoyacs@users.noreply.github.com>
2022-01-13 18:15:11 -0300

    diccionario

---
+++ bootcamp/JavaScript/Jugando con objetos/playObjects.js
@@ -0,0 +1,30 @@
+var users = [{name: "Michael", age:37}, {name: "John", age:15}, {name: "David", age:27}];
+
+// ¿Cómo harías print/log de la edad de John?
+console.log(users[1].age)
+
+// ¿Cómo harías print/log del nombre del primer objeto?
+console.log(users[0].name)
+
+// ¿Cómo harías print/log del nombre y la edad de cada usuario utilizando un for loop? Tu output debería verse algo como esto
+function printUsers(users){
+    for(let i=0; i<users.length; i++){
+        console.log(users[i].name +" - "+ users[i].age)
+    }
+}
+console.log("\nNombre - Edad")
+printUsers(users);
+
+// ¿Cómo harías para imprimir el nombre de los mayores de edad?
+
+
+function printUsers_adult(users){
+
+    for(let i=0; i<users.length; i++){
+        if(users[i].age > 17){
+        console.log(users[i].name+" - "+users[i].age)
+        }
+    }
+}
+console.log("\nAdultos: ")
+printUsers_adult(users);
