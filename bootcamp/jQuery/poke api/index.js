03ad2324670183e67167cdae57111e8a993eff10
Smoyacs <95979752+Smoyacs@users.noreply.github.com>
2022-01-14 01:12:44 -0300

    obteniendo pokemones

---
+++ bootcamp/jQuery/poke api/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    for (var i=1; i < 152; i++){
+        $('div.pokemones').append("<img src=http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+i+".png>");
+    }
+})
