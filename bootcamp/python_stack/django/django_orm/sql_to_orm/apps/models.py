20547ccab2111181d7aca499b04ba5d8553c39f6
Smoyacs <95979752+Smoyacs@users.noreply.github.com>
2022-02-21 09:39:44 -0300

    consultas sql a orm

---
+++ bootcamp/python_stack/django/django_orm/sql_to_orm/apps/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Wizard(models.Model):
+    name = models.CharField(max_length=45)
+    house = models.CharField(max_length=45)
+    pet = models.CharField(max_length=45)
+    year = models.IntegerField()
+    
+    def __str__(self):
+        return f"(<Wizard object: {self.name} {self.house} {self.pet} {self.year}>)"
